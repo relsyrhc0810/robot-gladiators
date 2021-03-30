ba043cda61d43cf5d455351c5cf2b1ae76f443a7
Chrysler Carreon <relsyrhc_0810@yahoo.com>
2021-03-29 21:55:21 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,14 +5,14 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+//console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function () {
+var fight = function (enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -66,6 +66,12 @@ var fight = function () {
         window.alert("You need to pick a valid option. Try again!");
     }
 };
-
 // run fight function to start game
 fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
+
+
+
